f99a196e349c72c728130f1b51947d7d0be40ebe
Ivan Rodriguez <ivan@ivans-mbp.lan>
2022-06-27 15:14:00 -0700

    added possible fix for proxy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import {createConection} from "./config/database.js"
 import users from "./api/users.js"
 import auth from "./api/auth.js"
+import cors from "cors"
 
 // Create connection to lowdb 
 createConection()
@@ -11,6 +12,7 @@ createConection()
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use(cors())
 // Initialize middleware
 app.use(express.json({extended:false}))
 
